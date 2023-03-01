cf5c6c2d3fb787f6994b40e4255e4c35e4a106f1
Koji Eguchi <koji.eguchi@hotmail.com>
2023-03-01 14:14:40 -0500

    Add loadTweets function to fetch JSON data from the server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,107 +5,66 @@
  */
 $(document).ready(function() {
 
-const createTweetElement = function(data) {
-  const {name, avatars, handle} = data.user;
-  const {text} = data.content;
-  const {created_at} = data;
-  const createdDate = new Date(created_at);
-  const currentDate = new Date();
-  const diffTime = Math.abs(currentDate - createdDate);
-  const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+  const createTweetElement = function(data) {
+    const {name, avatars, handle} = data.user;
+    const {text} = data.content;
+    const {created_at} = data;
+    const createdDate = new Date(created_at);
+    const currentDate = new Date();
+    const diffTime = Math.abs(currentDate - createdDate);
+    const days = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 
-  return `
-    <article>
-      <header>
-        <div class="profile-info">
-          <img src="${avatars}" alt="">
-          <p>${name}</p>
+    return `
+      <article>
+        <header>
+          <div class="profile-info">
+            <img src="${avatars}" alt="">
+            <p>${name}</p>
+          </div>
+          <p>${handle}</p>
+        </header>
+        <div class="tweet-content">
+          ${text}
         </div>
-        <p>${handle}</p>
-      </header>
-      <div class="tweet-content">
-        ${text}
-      </div>
-      <footer>
-        <p>${days} days ago</p>
-        <div class="icon-container">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-`};
+        <footer>
+          <p>${days} days ago</p>
+          <div class="icon-container">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+  `};
 
-const renderTweets = function(tweets) {
+  const renderTweets = function(tweets) {
 
 
-  for (const tweet of tweets) {
-    const $element = createTweetElement(tweet);
-    $('#tweets-container').append($element);
+    for (const tweet of tweets) {
+      const $element = createTweetElement(tweet);
+      $('#tweets-container').append($element);
+    }
   }
-}
 
+  $( "form" ).on( "submit", function( event ) {
+    event.preventDefault();
+    const serialized = $( this ).serialize();
 
-
-
-
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//   "created_at": 1461116232227
-// }
-
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1677506948784
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1677593348784
-  }
-];
-
-
-renderTweets(tweetData);
-
-// $('#submit-btn').click( event => {
-//   event.preventDefault();
-//   console.log( $( this ).serialize() );
-// });
-
-$( "form" ).on( "submit", function( event ) {
-  event.preventDefault();
-  const serialized = $( this ).serialize();
-
-
-  $.ajax({
-    type: "POST",
-    url: `/tweets`,
-    data: serialized,
+    $.ajax({
+      type: "POST",
+      url: `/tweets`,
+      data: serialized,
+    });
   });
-});
 
+  const loadtweets = function() {
+    $.ajax('/tweets', {method: 'GET'})
+    .then((tweetJson) => {
+      renderTweets(tweetJson);
+    });
+    
+  };
+
+  loadtweets();
 
 });
